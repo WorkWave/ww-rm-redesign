9d0ab2c1f4c7882e1dc5b6b0e0b254bd97f331df
Mike Lorzing <48525012+michaellorzing@users.noreply.github.com>
2021-11-16 13:20:06 -0500

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  siteMetadata: {
+    siteUrl: "https://www.yourdomain.tld",
+    title: "ww-rm-jamstack",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-source-sanity",
+      options: {
+        projectId: "6hmfnuhf",
+        dataset: "",
+      },
+    },
+  ],
+};
